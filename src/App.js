834d96a2c94278a8d926b425e39c2a62d8432e0e
Ahmed <ahmedreda152@gmail.com>
2022-01-17 17:59:53 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react';
 function App() {
   return (
     <Box>
-      <Text fontSize="6xl">(6xl) In love with React & Next</Text>
+      <Text fontSize="6xl">(6xl) In love with React Next</Text>
     </Box>
   );
 }
